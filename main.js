f0836c12d14f8106b329d738cb65acc2ac4fa25d
alby Wisum <albyally5@gmail.com>
2021-02-04 23:03:43 +0300

    Updates

---
+++ main.js
@@ -0,0 +1,19 @@
+// Selecting DOM elements
+
+const navToggle = document.querySelector('.nav__toggle');
+//const navigation = document.querySelector('.nav');
+const navLinks = document.querySelectorAll('.nav__link');
+
+// Add click event Listener to the selected elements
+
+navToggle.addEventListener('click', () => {
+    document.body.classList.toggle('close');
+    navToggle.style.outline = 'none';
+});
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('close');
+        //navigation.style.transitionDelay = '0s';
+    });
+});
